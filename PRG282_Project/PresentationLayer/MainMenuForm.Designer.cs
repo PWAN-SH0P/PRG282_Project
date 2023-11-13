3f8126999f37746bb934e1bb5f985857e8cbe8a9
PWAN-SH0P <107258691+PWAN-SH0P@users.noreply.github.com>
2023-11-13 17:20:09 +0200

    Put labels on main form

--- PRG282_Project/PresentationLayer/MainMenuForm.Designer.cs
+++ PRG282_Project/PresentationLayer/MainMenuForm.Designer.cs
@@ -42,6 +42,15 @@
             RegisterButton = new Button();
             DeregisterButton = new Button();
             ModuleView = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
             ((System.ComponentModel.ISupportInitialize)PictureBox).BeginInit();
             ((System.ComponentModel.ISupportInitialize)ModuleView).BeginInit();
             SuspendLayout();
@@ -142,7 +151,7 @@
             // 
             // RegisterButton
             // 
-            RegisterButton.Location = new Point(399, 179);
+            RegisterButton.Location = new Point(539, 172);
             RegisterButton.Name = "RegisterButton";
             RegisterButton.Size = new Size(94, 29);
             RegisterButton.TabIndex = 12;
@@ -152,7 +161,7 @@
             // 
             // DeregisterButton
             // 
-            DeregisterButton.Location = new Point(515, 179);
+            DeregisterButton.Location = new Point(665, 172);
             DeregisterButton.Name = "DeregisterButton";
             DeregisterButton.Size = new Size(94, 29);
             DeregisterButton.TabIndex = 13;
@@ -163,18 +172,108 @@
             // ModuleView
             // 
             ModuleView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            ModuleView.Location = new Point(358, 47);
+            ModuleView.Location = new Point(502, 50);
             ModuleView.Name = "ModuleView";
             ModuleView.RowHeadersWidth = 51;
             ModuleView.RowTemplate.Height = 29;
             ModuleView.Size = new Size(300, 104);
             ModuleView.TabIndex = 15;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(209, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(118, 20);
+            label1.TabIndex = 16;
+            label1.Text = "Student Number";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(209, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 20);
+            label2.TabIndex = 17;
+            label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(209, 120);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 20);
+            label3.TabIndex = 18;
+            label3.Text = "Surname";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(209, 169);
+            label4.Name = "label4";
+            label4.Size = new Size(127, 20);
+            label4.TabIndex = 19;
+            label4.Text = "Picture of Student";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(209, 219);
+            label5.Name = "label5";
+            label5.Size = new Size(94, 20);
+            label5.TabIndex = 20;
+            label5.Text = "Date of Birth";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(209, 251);
+            label6.Name = "label6";
+            label6.Size = new Size(57, 20);
+            label6.TabIndex = 21;
+            label6.Text = "Gender";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(209, 280);
+            label7.Name = "label7";
+            label7.Size = new Size(50, 20);
+            label7.TabIndex = 22;
+            label7.Text = "Phone";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(209, 317);
+            label8.Name = "label8";
+            label8.Size = new Size(105, 20);
+            label8.TabIndex = 23;
+            label8.Text = "Street Address";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(623, 27);
+            label9.Name = "label9";
+            label9.Size = new Size(66, 20);
+            label9.TabIndex = 24;
+            label9.Text = "Modules";
+            // 
             // MainMenuForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(897, 534);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(ModuleView);
             Controls.Add(DeregisterButton);
             Controls.Add(RegisterButton);
@@ -213,5 +312,14 @@
         private Button RegisterButton;
         private Button DeregisterButton;
         private DataGridView ModuleView;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
     }
 }
